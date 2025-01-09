2d72f1a057ddb4df07988afb56e0bfea2ad73f9a
Mateus-Pasqueta <Mateuspasquetap@outlook.com>
2025-01-09 17:36:42 -0300

    fix: upsert transaction function

--- app/_actions/upsert-transaction/index.ts
+++ app/_actions/upsert-transaction/index.ts
@@ -27,9 +27,6 @@ export const upsertTransaction = async (params: UpsertTransactionParams) => {
     throw new Error("Unauthorized");
   }
   await db.transaction.upsert({
-    where: {
-      id: params?.id ?? "",
-    },
     update: {
       ...params,
       userId,
@@ -37,6 +34,9 @@ export const upsertTransaction = async (params: UpsertTransactionParams) => {
     create: {
       ...params,
       userId,
+    },
+    where: {
+      id: params?.id ?? "",
     },
   });
   revalidatePath("/transactions");
